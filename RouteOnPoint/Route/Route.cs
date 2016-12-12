916cc95a8561e75ec27a1d8d1b91fecbb8a57054
Sascha Worms <sworms@avans.nl>
2016-12-12 10:56:45 +0100

    POI en Route

--- RouteOnPoint/Route/Route.cs
+++ RouteOnPoint/Route/Route.cs
@@ -8,5 +8,34 @@ namespace RouteOnPoint.Route
 {
     class Route
     {
+        private string _name;
+        private List<POI> _points;
+        public Route(string name, List<POI> _points)
+        {
+            this._name = name;
+            this._points = _points;
+        }
+        public Route(string name)
+        {
+            this._name = name;
+         }
+
+        public void addPOI(POI p)
+        {
+            if (!_points.Contains(p)) {
+                _points.Add(p);
+            }
+        }
+        public void addPOI(POI p, int plekInLijst)
+        {
+            if (!_points.Contains(p))
+            {
+                _points.Insert(plekInLijst,p);
+            }
+        }
+        public void setPOIList(List<POI> points)
+        {
+            _points = points;
+        }
     }
 }
