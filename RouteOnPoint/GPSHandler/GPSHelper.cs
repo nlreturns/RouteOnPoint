42ce1f9af3b921773fb879d0a6b5260ccf92a00a
DESKTOP-S8PIJ7G\Joey <jvanhoek@hotmail.com>
2016-12-12 10:42:33 +0100

    few methods

---
+++ RouteOnPoint/GPSHandler/GPSHelper.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RouteOnPoint.GPSHandler
+{
+    class GPSHelper
+    {
+        public GPSReader Gpsreader;
+
+        public GPSHelper()
+        {
+
+        }
+
+        public double? DistanceToPOI( /*POI point, GeoCoordinate coord*/)
+        {
+            return null;
+        }
+
+        public TimeSpan? TimeToPOI(/*POI point, GeoCoordinate coord*/)
+        {
+            return null;
+        }
+    }
+}
