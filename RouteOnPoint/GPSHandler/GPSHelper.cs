6608f0607e317e153ee5aefd77afb349be2510ef
DESKTOP-S8PIJ7G\Joey <jvanhoek@hotmail.com>
2016-12-12 11:47:30 +0100

    time

--- RouteOnPoint/GPSHandler/GPSHelper.cs
+++ RouteOnPoint/GPSHandler/GPSHelper.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Windows.Devices.Geolocation;
 
 namespace RouteOnPoint.GPSHandler
 {
@@ -15,14 +17,25 @@ namespace RouteOnPoint.GPSHandler
 
         }
 
-        public double? DistanceToPOI( /*POI point, GeoCoordinate coord*/)
+        //returns distance via the map in meters
+        public double? DistanceToPOI( /*POI point,*/ Geocoordinate coord)
         {
             return null;
         }
 
-        public TimeSpan? TimeToPOI(/*POI point, GeoCoordinate coord*/)
+        public static TimeSpan? TimeToPOI(/*POI point, Geocoordinate coord*/)
         {
-            return null;
+           // double metersToPoint = DistanceToPOI(point, coord);
+            int timeInSeconds = (int)Math.Floor((20.0 / 1.39));
+            Debug.WriteLine(timeInSeconds);
+            
+
+
+
+            // s/v = t
+            //5 km per uur
+            return new TimeSpan();
+
         }
     }
 }
