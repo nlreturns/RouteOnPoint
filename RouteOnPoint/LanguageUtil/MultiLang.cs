817c304ce1a89a300eef6c5a24f24c007454efed
RickvF <pajfesse@avans.nl>
2016-12-12 10:30:14 +0100

    no message

---
+++ RouteOnPoint/LanguageUtil/MultiLang.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RouteOnPoint.LanguageUtil
+{
+    public class MultiLang
+    {
+        private Dictionary<string, string> _Englisch;
+        private Dictionary<string, string> _Dutch;
+
+        public enum LanguageEnum { Englisch, Dutch};
+
+        public LanguageEnum Language;
+
+        public MultiLang()
+        {
+
+        } 
+
+        
+    }
+}
