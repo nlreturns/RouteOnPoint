a6ec4081122bb79703848fce2b4acc220dd45256
RickvF <pajfesse@avans.nl>
2016-12-15 13:41:37 +0100

    Test uitgevoerd en succesvol afgerond

--- RouteOnPoint/LanguageUtil/TestMultiLang.cs
+++ RouteOnPoint/LanguageUtil/TestMultiLang.cs
@@ -16,7 +16,7 @@ namespace RouteOnPoint.LanguageUtil
         private async void setUP()
         {
             await MultiLang.setLanguage(MultiLang.LanguageEnum.English);
-            string name = MultiLang.GetContent("P_ANTONIUSVANPADUAKERK_NAME");
+            string name = MultiLang.GetContent("R_HISTORISCHEKILOMETER_NAME");
             string name2 = MultiLang.GetContent("P_BEGINHAVERMARKT_NAME");
             string name3 = MultiLang.GetContent("P_BEGIJNENHOF_NAME");
             string name4 = MultiLang.GetContent("P_KASTEELVANBREDA_INFO");
@@ -24,6 +24,16 @@ namespace RouteOnPoint.LanguageUtil
             string name6 = MultiLang.GetContent("R_BLINDWALLS_NAME");
             string name7 = MultiLang.GetContent("P_DZIA_NAME");
             string name8 = MultiLang.GetContent("P_BENEINE_NAME");
+
+            await MultiLang.setLanguage(MultiLang.LanguageEnum.Dutch);
+            string name01 = MultiLang.GetContent("R_HISTORISCHEKILOMETER_NAME");
+            string name21 = MultiLang.GetContent("P_BEGINHAVERMARKT_NAME");
+            string name31 = MultiLang.GetContent("P_BEGIJNENHOF_NAME");
+            string name41 = MultiLang.GetContent("P_KASTEELVANBREDA_INFO");
+            string name51 = MultiLang.GetContent("ROUTESELECTIONVIEWMODEL_SELECTROUTE_TEXT");
+            string name61 = MultiLang.GetContent("R_BLINDWALLS_NAME");
+            string name71 = MultiLang.GetContent("P_DZIA_NAME");
+            string name81 = MultiLang.GetContent("P_BENEINE_NAME");
         }
 
     }
