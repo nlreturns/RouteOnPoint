d5a976ccad6e5ed3c0db853f980c388c8969d7c7
Sascha Worms <sworms@avans.nl>
2016-12-21 10:50:38 +0100

    Route selectie verbeterd

--- RouteOnPoint/Pages/RouteSelectionViewModel.xaml.cs
+++ RouteOnPoint/Pages/RouteSelectionViewModel.xaml.cs
@@ -28,7 +28,7 @@ namespace RouteOnPoint.Pages
     {
         Frame rootFrame = Window.Current.Content as Frame;
         Route.Route route;
-
+        private Route.Route loadedRoute =null;
         public RouteSelectionViewModel()
         {
             this.InitializeComponent();
@@ -39,19 +39,23 @@ namespace RouteOnPoint.Pages
             SystemNavigationManager.GetForCurrentView().BackRequested += App_BackRequested;
         }
 
-        private void Click(object sender, TappedRoutedEventArgs e)
+        private async void Click(object sender, TappedRoutedEventArgs e)
         {
     
             Grid g = (Grid)sender;
-            Route.Route r;
+            Route.Route r =null;
             switch (g.Name)
             {
-                case "BlindWalls":
+                case "Blind":
                     r = new RouteHelper().createBlindWalls();
                     break;
-                default:
+                case "Kilo":
                     r = new RouteHelper().createHistoriscRoute();
                     break;
+                case "Resume":
+                    r = loadedRoute;
+                    break;              
+
             }
             GPSReader.route = r;
             rootFrame.Navigate(typeof(RouteViewModel));
@@ -71,8 +75,23 @@ namespace RouteOnPoint.Pages
             }
         }
 
-        protected override void OnNavigatedTo(NavigationEventArgs e)
+        protected async override void OnNavigatedTo(NavigationEventArgs e)
         {
+            RouteHandler load = new RouteHandler();
+            try
+            {
+                loadedRoute = await load.GetRouteFromFile("RouteData.json");
+            }
+            catch (Exception)
+            {
+                loadedRoute = null;
+            }
+            if (loadedRoute == null || loadedRoute._points.Count()<1)
+            {
+                ResumeButton.Visibility = Visibility.Collapsed;
+                Resume.Visibility = Visibility.Collapsed;
+                
+            }
             SystemNavigationManager.GetForCurrentView().AppViewBackButtonVisibility =
                 AppViewBackButtonVisibility.Visible;
         }
